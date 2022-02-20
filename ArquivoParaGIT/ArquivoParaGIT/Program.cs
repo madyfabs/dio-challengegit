90bf8358ebaeb8983bc9f72c3803f34521f035ea
madyfabs <fabriciio_@live.com>
2022-02-19 22:33:04 -0400

    ArquivoParaGIT/

---
+++ ArquivoParaGIT/ArquivoParaGIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArquivoParaGIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arquivo simples em C# para o desafio GIT DIO!");
+        }
+    }
+}
